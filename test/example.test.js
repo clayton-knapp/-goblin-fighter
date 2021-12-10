f5096d48f8b48e49db7282037bbdfa2010a7562f
Clayton Knapp <csjknapp@gmail.com>
2021-12-10 10:25:17 -0800

    fixed TDD

--- test/example.test.js
+++ test/example.test.js
@@ -7,7 +7,7 @@ const test = QUnit.test;
 test('tests if renderGoblin returns correct DOM node', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<div class="goblin-container"><p>Meep!</p><p>HP: 3</p><p class="emoji">👹</p></div>';
+    const expected = '<div class="goblin-container"><p>Meep!</p><p>HP: 3</p><p class="emoji"></p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -15,11 +15,11 @@ test('tests if renderGoblin returns correct DOM node', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected, 'successfully returns "<div class="goblin-container"><p>Meep!</p><p>HP: 3</p><p class="emoji">👹</p></div>"');
+    expect.equal(actual.outerHTML, expected, 'successfully returns "<div class="goblin-container"><p>Meep!</p><p>HP: 3</p><p class="emoji"></p></div>"');
 
     //Arrange
     // Set up your arguments and expectations
-    const expected2 = '<div class="goblin-container"><p>Jerry</p><p>HP: 1</p><p class="emoji">👹</p></div>';
+    const expected2 = '<div class="goblin-container"><p>Jerry</p><p>HP: 1</p><p class="emoji"></p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -27,5 +27,5 @@ test('tests if renderGoblin returns correct DOM node', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual2.outerHTML, expected2, 'successfully returns "<div class="goblin-container"><p>Jerry</p><p>HP: 1</p><p class="emoji">👹</p></div>"');
+    expect.equal(actual2.outerHTML, expected2, 'successfully returns "<div class="goblin-container"><p>Jerry</p><p>HP: 1</p><p class="emoji"></p></div>"');
 });
